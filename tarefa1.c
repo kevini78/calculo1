283257bdd78200e154da64a9ed62ba3cb93381d8
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-08-16 20:56:18 -0300

    Add files via upload

---
+++ tarefa1.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+int main(){
+    int num1;
+    int num2;
+    int resultadoSoma;
+    int resultadoSubtracao;
+    int resultadoDivisao;
+    int resultadoMultiplicacao;
+printf("Digite o primeiro numero %d", num1);
+scanf("%d",&num1);
+printf("Digite o segundo numero %d", num2);
+scanf("%d",&num2);
+resultadoSoma = num1 + num2;
+resultadoSubtracao = num1 - num2;
+resultadoMultiplicacao = num1 * num2;
+resultadoDivisao = num1 / num2;
+    printf("\nResultado soma: %d", resultadoSoma);
+    printf("\nResultado subtracao: %d", resultadoSubtracao);
+    printf("\nResultado divisao: %d", resultadoDivisao);
+    printf("\nResultado multiplicacao: %d", resultadoMultiplicacao);
+
+
+
+
+
+}
